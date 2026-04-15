39fc11d77f56c79fbf9187408960ca3b8dcc67cd
AsepSaniNugraha <asepsaninugraha@gmail.com>
2026-04-15 19:54:09 +0700

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Monchrome--Educational-Dashboard/',
   plugins: [react(), tailwindcss()],
 })
